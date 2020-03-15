2121406bd96e3b52d41ff5c1e8faf53cffced7cf
Myanmar Data Science <59246204+myanmards@users.noreply.github.com>
2020-03-15 15:30:05 +0800

    employees training data

---
+++ intro-das.sql
@@ -0,0 +1,91 @@
+-- --------------------------------------------------------
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `emp_id` int(11) NOT NULL,
+  `birth_date` date NOT NULL,
+  `first_name` varchar(14) NOT NULL,
+  `last_name` varchar(16) NOT NULL,
+  `gender` enum('M','F') NOT NULL,
+  `hire_date` date NOT NULL
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`emp_id`);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salaries`
+--
+
+CREATE TABLE `salaries` (
+  `emp_id` int(11) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `effect_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `salaries`
+--
+ALTER TABLE `salaries`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Constraints for table `salaries`
+--
+ALTER TABLE `salaries`
+  ADD CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`emp_id`) ON DELETE CASCADE;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `dept_id` char(4) NOT NULL,
+  `dept_name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`dept_id`),
+  ADD UNIQUE KEY `dept_name` (`dept_name`);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dept_emp`
+--
+
+CREATE TABLE `dept_emp` (
+  `emp_id` int(11) NOT NULL,
+  `dept_id` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `dept_emp`
+--
+ALTER TABLE `dept_emp`
+  ADD PRIMARY KEY (`emp_id`,`dept_id`),
+  ADD KEY `dept_no` (`dept_id`);
+
+--
+-- Constraints for table `dept_emp`
+--
+ALTER TABLE `dept_emp`
+  ADD CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`emp_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `dept_emp_ibfk_2` FOREIGN KEY (`dept_id`) REFERENCES `departments` (`dept_id`) ON DELETE CASCADE;
+
+-- --------------------------------------------------------
